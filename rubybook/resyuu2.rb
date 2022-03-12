6f98837d05191f431835aab910107dd144b73fcb
shinshinshin123 <lingmushennaijie@gmail.com>
2022-03-12 21:12:50 +0900

    練習問題関連

---
+++ rubybook/resyuu2.rb
@@ -0,0 +1,62 @@
+# 問1
+puts 2 + 3
+
+# 問２
+# puts 2.0 * 2.0 * 3.14
+puts 2 * 2 * 3.14
+
+# 問３
+puts "Ruby"
+
+# 問４
+puts "abc" + "def"
+
+# 問５
+x = "123"
+y = "456"
+puts x.to_i + y.to_i
+
+#参考回答
+# puts "123".to_i + "456".to_i
+
+#問６
+coffee = 300
+espresso = 100
+
+puts "コーヒー:" + coffee + "円"
+puts coffee + (espresso * 2) + "円"
+
+#回答
+# coffee = 300
+# espresso = 100
+
+# puts "コーヒー: #{coffee}円"
+# puts "合計: #{coffee + espresso * 2}円"
+
+#問７
+coffee = 300 + 400
+espresso = 100
+
+puts "コーヒー:" + coffee + "円"
+puts coffee + (espresso * 2) + "円"
+
+#回答
+# coffee = 400
+# espresso = 100
+
+# puts "コーヒー: #{coffee}円"
+# puts "合計: #{coffee + espresso * 2}円"
+
+#問8
+total = 300 * 2
+tax = total * 0.08
+p tax
+puts tax
+
+#回答
+# s
+
+# 問9
+
+total = 300 * 2
+puts total
